3b5a2d1bbc8bff35ec7f119f3db2ff12c7734fd9
bsteves <bsteves@gmail.com>
2014-04-10 15:31:17 -0700

    data output

--- lakewatch_data.R
+++ lakewatch_data.R
@@ -19,25 +19,43 @@ organisms <- dbGetQuery(con, "SELECT id as organism_id, user_id, confidence, sam
 postgresqlCloseConnection(con)
 
 
-
-sites <- sites %.% inner_join(users, by="user_id") %.% inner_join(lakes, by="lake_id")
-dat <- sites %.% inner_join(samplings, by="site_id") %.% inner_join(physical_data, by="sampling_id")
-dat$sampling_name <- paste(dat$lake_name, dat$site_name, sep="-")
+dat <- sites %.% inner_join(users, by="user_id") %.% inner_join(lakes, by="lake_id") %.% inner_join(samplings, by="site_id") %.% inner_join(physical_data, by="sampling_id") %.% group_by(site_name)
 
 
-for (i in 1:nrow(sites)){
-  site_dat<-dat %.% filter(site_id == sites$site_id[i] & !is.na(value))
-  
-  if (nrow(site_dat) < 2) next
-  site_dat<-site_dat %.% arrange(sampling_date, depth)
-  
+write.csv("OLW_users.csv", users)
+
+
+
+
+
+# create a temperature profile plot function
+lakeTempPlot <- function(site_dat){ 
+  sampling_name <- paste(site_dat$lake_name.x[1], site_dat$site_name[1], sep="-")
   p <- ggplot(data=site_dat, aes(y=value, x=depth, group=sampling_date))
   p <- p + geom_line(aes(color=factor(sampling_date))) 
-  p <- p + labs(title=site_dat$sampling_name, y=expression(Temperature~(degree~C)), x="Depth (m)")
+  p <- p + labs(title=sampling_name, y=expression(Temperature~(degree~C)), x="Depth (m)")
   p <- p + scale_x_reverse()  + coord_flip() + scale_color_discrete(name="Sampling Date")
   p <- p + theme_bw()
-  print(p)
-  
+  print(p) 
 }
+
+
+# plot the temperature data for each site
+plots<-dat  %.% do(lakeTempPlot)
+
+# for (i in 1:nrow(sites)){
+#   site_dat<-dat %.% filter(site_id == sites$site_id[i] & !is.na(value))
+#   
+#   if (nrow(site_dat) < 2) next
+#   site_dat<-site_dat %.% arrange(sampling_date, depth)
+#   
+#   p <- ggplot(data=site_dat, aes(y=value, x=depth, group=sampling_date))
+#   p <- p + geom_line(aes(color=factor(sampling_date))) 
+#   p <- p + labs(title=site_dat$sampling_name, y=expression(Temperature~(degree~C)), x="Depth (m)")
+#   p <- p + scale_x_reverse()  + coord_flip() + scale_color_discrete(name="Sampling Date")
+#   p <- p + theme_bw()
+#   print(p)
+#   
+# }
 
 
